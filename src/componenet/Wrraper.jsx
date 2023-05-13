85f321dbd8b0667065747d6f07066854bc091bc1
badr ghanbi <bghanbi50@gmail.com>
2023-05-13 16:33:59 +0100

    add style o the wrraper componenet

---
+++ src/componenet/Wrraper.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+// a wrapper componenet for each section
+const Wrraper = ({children}) => {
+  return (
+    <div className='bg-white rounded-md'>{children}</div>
+  )
+}
+
+export default Wrraper
