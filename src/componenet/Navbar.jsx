af436f01eaf2bd1b2db84abc53b73a593f5299ed
badr ghanbi <bghanbi50@gmail.com>
2023-05-13 18:57:32 +0100

    created the navbar componeenet

--- src/componenet/Navbar.jsx
+++ src/componenet/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Wrraper from './Wrraper'
 import Btn from './Btn'
-
+import MoonIcon from "../../src/moon-solid.svg"
 const Navbar = () => {
     return(
    <Wrraper >
     <nav className='flex  justify-between items-center'>
     <h1 className='text-2xl font-bold'>Cryptobase</h1>
-    <div>
+    <div className='flex'>
+     <img className='h-6 mx-3' src={MoonIcon} alt="moon icon" /> 
     <h4>Dark Mode</h4>
     </div>
     {/* sign in and sign up */}
