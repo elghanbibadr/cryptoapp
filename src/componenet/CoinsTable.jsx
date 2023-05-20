cd719d603442a4263f49c7ec55a64fe1df0fa477
badr ghanbi <bghanbi50@gmail.com>
2023-05-20 12:30:48 +0100

    wrrape the main layout with a wrraper componenet add a shadow to it

--- src/componenet/CoinsTable.jsx
+++ src/componenet/CoinsTable.jsx
@@ -1,31 +1,32 @@
-import React, { useState ,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import CoinItem from './CoinItem'
 import { data } from '../data'
-
+import Wrraper from "../componenet/Wrraper"
 export const CoinsTable = () => {
-   
+
 
 
   return (
-    <table className='w-full '>
-        {/* the table header  */}
-    <tr className=' my-20 w-full  m-[2rem]'>
-      <th></th>
-      <th className='text-end'>#</th>
-      <th>coin</th>
-      <th>price</th>
-      <th>24h</th>
-      <th>24h Volume</th>
-      <th>mkt</th>
-      <th>last 7 Days</th>
-    </tr>
- {console.log(data)}
-
-    {/* table data row */}
-   { data.map(({id,market_cap_rank,image,name,symbol,sparkline,current_price,price_change_percentage_24h,total_volume,market_cap}) => {
-    return <CoinItem key={id} market_cap_rank={market_cap_rank} sparkline={sparkline} image={image} name={name} symbol={symbol} current_price={current_price} price_change_percentage_24h={price_change_percentage_24h} total_volume={total_volume} market_cap={market_cap} />
-   })}
-    
-  </table>
+    <Wrraper>
+      <table className='w-full '>
+        <thead>
+          <tr className=' my-20 w-full  m-[2rem]'>
+            <th></th>
+            <th className='text-end'>#</th>
+            <th>coin</th>
+            <th>price</th>
+            <th>24h</th>
+            <th>24h Volume</th>
+            <th>mkt</th>
+            <th>last 7 Days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(({ id, market_cap_rank, image, name, symbol, sparkline, current_price, price_change_percentage_24h, total_volume, market_cap }) => {
+            return <CoinItem key={id} market_cap_rank={market_cap_rank} sparkline={sparkline} image={image} name={name} symbol={symbol} current_price={current_price} price_change_percentage_24h={price_change_percentage_24h} total_volume={total_volume} market_cap={market_cap} />
+          })}
+        </tbody>
+      </table>
+    </Wrraper>
   )
 }
--- src/componenet/Wrraper.jsx
+++ src/componenet/Wrraper.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 // a wrapper componenet for each section
 const Wrraper = ({children}) => {
   return (
-    <div className='bg-white rounded-md'>{children}</div>
+    <div className='bg-white shadow-xl p-4 rounded-md'>{children}</div>
   )
 }
 
