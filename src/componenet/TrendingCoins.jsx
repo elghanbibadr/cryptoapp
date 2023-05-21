e4a89da177ff7a37ac7a43470f2d0d670ac208c4
badr ghanbi <bghanbi50@gmail.com>
2023-05-21 16:27:35 +0100

    create the base structure of the trending componenet

--- src/componenet/TrendingCoins.jsx
+++ src/componenet/TrendingCoins.jsx
@@ -4,17 +4,24 @@ import { data } from '../data'
 const TrendingCoins = () => {
   return (
     <Wrraper>
-        <h1 className='text-black text-xl font-bold'>TrendingCoins</h1>
-        {data.slice(4,8).map(({id,name,symbol,image})=>{
-            return <Wrraper className="m-2" key={id}>
-            <div>
-              <img className='h-10 w-10 float-left' src={image} alt="coin image" />
-              <p>{name}</p>
-              <p>{symbol}</p>
+        <h1 className='text-black text-xl mb-8 font-bold'>TrendingCoins</h1>
+        <div className='grid grid-cols-3'>
+        {data.slice(3,8).map(({id,name,symbol,image,high_24h})=>{
+                return <Wrraper className="m-2 flex justify-between" key={id}>
+                <div className='flex '>
+                  <img className='h-10 w-10 mr-6 ' src={image} alt="coin image" />
+                  <div>
+                      <p>{name}</p>
+                      <p>{symbol}</p>
+                  </div>
+                </div>
+                <div>
+                    <p>{high_24h}</p>
+                </div>
+                </Wrraper>
+                        }) }
             </div>
-            </Wrraper>
-        }) }
-    </Wrraper>
+                    </Wrraper>
   )
 }
 
