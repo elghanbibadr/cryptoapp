4e4784e13ea15cb49d14a9645bb4101529de3b67
badr ghanbi <bghanbi50@gmail.com>
2023-05-21 16:44:05 +0100

    create the remaining nedded styles for the trending coin componenet

--- src/componenet/TrendingCoins.jsx
+++ src/componenet/TrendingCoins.jsx
@@ -7,7 +7,7 @@ const TrendingCoins = () => {
         <h1 className='text-black text-xl mb-8 font-bold'>TrendingCoins</h1>
         <div className='grid grid-cols-3'>
         {data.slice(3,8).map(({id,name,symbol,image,high_24h})=>{
-                return <Wrraper className="m-2 flex justify-between" key={id}>
+                return <Wrraper className="m-2 flex justify-between items-center" key={id}>
                 <div className='flex '>
                   <img className='h-10 w-10 mr-6 ' src={image} alt="coin image" />
                   <div>
@@ -15,7 +15,8 @@ const TrendingCoins = () => {
                       <p>{symbol}</p>
                   </div>
                 </div>
-                <div>
+                <div className='flex'>
+                    <span className='bg-orange-300 h-5 w-5 text-white inline-flex justify-center items-center mx-1 rounded-full'>$</span>
                     <p>{high_24h}</p>
                 </div>
                 </Wrraper>
