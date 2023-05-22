503519cbc1e2c4fdcff5a0008bce9a31b0b8e828
badr ghanbi <bghanbi50@gmail.com>
2023-05-22 13:40:10 +0100

    wrrape the trending coin card to link componenet to that link it to the product detail route'

--- src/componenet/TrendingCoins.jsx
+++ src/componenet/TrendingCoins.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
 import Wrraper from './Wrraper'
+import { Link } from 'react-router-dom'
 import { data } from '../data'
+
 const TrendingCoins = () => {
   return (
     <Wrraper className='mt-10'>
-        <h2 className='text-black text-xl mb-20 font-bold'>TrendingCoins</h2>
-        <div className='grid grid-cols-3'>
-        {data.slice(3,8).map(({id,name,symbol,image,high_24h})=>{
-                return <Wrraper className="m-2 flex justify-between items-center" key={id}>
-                <div className='flex '>
-                  <img className='h-10 w-10 mr-6 ' src={image} alt="coin image" />
-                  <div>
-                      <p>{name}</p>
-                      <p>{symbol}</p>
-                  </div>
+      <h2 className='text-black text-xl mb-20 font-bold'>TrendingCoins</h2>
+      <div className='grid grid-cols-3'>
+        {data.slice(3, 8).map(({ id, name, symbol, image, high_24h }) => {
+    return <Link to={`/coin/${id}`} >
+            <Wrraper className="m-2 flex justify-between items-center" key={id}>
+              <div className='flex '>
+                <img className='h-10 w-10 mr-6 ' src={image} alt="coin image" />
+                <div>
+                  <p>{name}</p>
+                  <p>{symbol}</p>
                 </div>
-                <div className='flex'>
-                    <span className='bg-orange-300 h-5 w-5 text-white inline-flex justify-center items-center mx-1 rounded-full'>$</span>
-                    <p>{high_24h}</p>
-                </div>
-                </Wrraper>
-                        }) }
-            </div>
-                    </Wrraper>
+              </div>
+              <div className='flex'>
+                <span className='bg-orange-300 h-5 w-5 text-white inline-flex justify-center items-center mx-1 rounded-full'>$</span>
+                <p>{high_24h}</p>
+              </div>
+            </Wrraper>
+          </Link>
+        })}
+      </div>
+    </Wrraper>
   )
 }
 
