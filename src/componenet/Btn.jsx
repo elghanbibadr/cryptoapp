7d7b65b77fb6c4ad3743ab37e84bdcdba2d2b360
badr ghanbi <bghanbi50@gmail.com>
2023-05-23 15:03:50 +0100

    create the watchlist componenet

--- src/componenet/Btn.jsx
+++ src/componenet/Btn.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Btn = ({text,className}) => {
+const Btn = ({text,className,onClick}) => {
   return (
-    <button className={` rounded-xl font-medium px-3 mx-1 py-2 ${className}`}>{text}</button>
+    <button onClick={onClick} className={` rounded-xl font-medium px-3 mx-1 py-2 ${className}`}>{text}</button>
   )
 }
 
--- src/routes/Acount.jsx
+++ src/routes/Acount.jsx
@@ -2,20 +2,39 @@ import React from 'react'
 import Wrraper from '../componenet/Wrraper'
 import Btn from '../componenet/Btn'
 import { UserAuth } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 const Acount = () => {
-  const  {user} =UserAuth()
-  console.log(user.email)
+  const  {user,logout} =UserAuth()
+  const navigate=useNavigate()
+
+  const handleSignOut=async () => {
+    try{
+   await logout()
+   navigate('/')
+    }catch(e){
+  
+    }
+
+  }
+  console.log(user)
   return (
+    <>
     <Wrraper>
       <div className='flex justify-between items-center p-3'>
         <div>
           <h2 className='font-bold'>Account</h2>
-          <p className='mt-4'>Welcome {user.email}</p>
+          <p className='mt-4'>Welcome   {user?.email}</p>
         </div>
-             <Btn text="Sign out"  className="bg-white shadow-md" />
+             <Btn onClick={handleSignOut} text="Sign out"  className="bg-white shadow-md" />
       </div>
     </Wrraper>
+    <Wrraper className='mt-6 h-[200px]'> 
+    <h2 className='font-bold'>Watch List</h2>
+    <p>you don't have any coins saved please save a coin to add it to your watch list . click here to search coin</p>
+    </Wrraper>
+  
+    </>
   )
 }
 
