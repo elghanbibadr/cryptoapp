26259d14da54d9abb458c85a65f7246d408c06e6
badr ghanbi <bghanbi50@gmail.com>
2023-05-23 15:13:03 +0100

    attache email and password states to the sign in componenet

--- src/routes/SignIn.jsx
+++ src/routes/SignIn.jsx
@@ -1,27 +1,43 @@
 import React from 'react'
-// import {AiOutlineMail} from 'react-icons'
 import { Link } from 'react-router-dom'
 import Btn from '../componenet/Btn'
+import { UserAuth } from '../context/AuthContext'
 
 const SignIn = () => {
-  const handleSignIn=() => alert('Sign In')
+  const {signIn,user}=UserAuth()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error,setError]=useState('') ;
+
+    const handleSubmit=async (e) => {
+      e.preventDefault()
+      setError('')
+      try{
+       await signIn(email,password)
+      }catch(e){
+          setError(e.message)
+      }
+      console.log(user?.email)
+  }
+  
   
   return (
     <div>
-      {/* <AiOutlineMail /> */}
       <div className='max-w-[400px] mx-auto min-h-[600px] px-4 py-20'>
         <h1 className='text-2xl font-bold'>Sign In</h1>
-        <form onSubmit={handleSignIn} >
+        {error ? <p className='bg-red-300 p-3 my-2'>{error}</p> :null }
+
+        <form onSubmit={handleSubmit} >
           <div className="my-4">
             <label>email</label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
-              <input className="w-full p-2 bg-primary border border-input rounded-2xl" type='email' placeholder='enter your email'/>
+              <input onChange={(e)=>setEmail(e.target)} className="w-full p-2 bg-primary border border-input rounded-2xl" type='email' placeholder='enter your email'/>
             </div>
           </div>
           <div className="my-4">
             <label>Password</label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
-              <input className="w-full p-2 bg-primary border border-input rounded-2xl" type='password' placeholder='enter your password'/>
+              <input onChange={(e) => setPassword(e.target)} className="w-full p-2 bg-primary border border-input rounded-2xl" type='password' placeholder='enter your password'/>
             </div>
           </div>
           <Btn text="Sign In" className="bg-blue-400  my-6 w-full text-white" />
--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -11,7 +11,6 @@ const SignUp = () => {
     }
 
     const {signUp}=UserAuth()
-    console.log(signUp)
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error,setError]=useState('') ;
